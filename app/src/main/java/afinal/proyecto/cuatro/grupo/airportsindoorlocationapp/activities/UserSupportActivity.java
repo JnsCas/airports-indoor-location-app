86fad45356eac18965936c4b945a1c244cf90a56
evincelli <ernesto.vincelli@mercadolibre.com>
2018-09-16 20:52:10 -0300

    user_support_layout_v3

--- app/src/main/java/afinal/proyecto/cuatro/grupo/airportsindoorlocationapp/activities/UserSupportActivity.java
+++ app/src/main/java/afinal/proyecto/cuatro/grupo/airportsindoorlocationapp/activities/UserSupportActivity.java
@@ -1,9 +1,11 @@
 package afinal.proyecto.cuatro.grupo.airportsindoorlocationapp.activities;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,6 +17,7 @@ import com.google.gson.Gson;
 
 import org.json.JSONObject;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -40,24 +43,42 @@ public class UserSupportActivity extends AppCompatActivity {
         vuelosSpinner = findViewById(R.id.usersupport_vuelos_sp);
 
         vuelosSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @SuppressLint("ResourceAsColor")
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
                 Vuelo vuelo = listaVuelos.get(position);
 
-                TextView numeroTV = findViewById(R.id.usersupport_result_numero);
-                numeroTV.setText(vuelo.getNumber());
+                //TextView numeroTV = findViewById(R.id.usersupport_result_numero);
+                //numeroTV.setText(vuelo.getNumber());
 
-                TextView destinoTV = findViewById(R.id.usersupport_result_destino);
+                TextView destinoTV;
+                destinoTV = findViewById(R.id.usersupport_result_destino);
                 destinoTV.setText(vuelo.getDestination().toString());
 
-                TextView fechaEmbarqueTV = findViewById(R.id.usersupport_result_fechaEmbarque);
-                fechaEmbarqueTV.setText(vuelo.getBoardingDateTime().toString());
+                TextView fechaEmbarqueTV;
+                fechaEmbarqueTV = findViewById(R.id.usersupport_result_fechaEmbarque);
+                android.text.format.DateFormat df = new android.text.format.DateFormat();
+                fechaEmbarqueTV.setText(df.format("yyyy-MM-dd hh:mm A", vuelo.getBoardingDateTime()));
 
-                TextView puertaEmbarqueTV = findViewById(R.id.usersupport_result_puertaEmbarque);
+                TextView puertaEmbarqueTV;
+                puertaEmbarqueTV = findViewById(R.id.usersupport_result_puertaEmbarque);
                 puertaEmbarqueTV.setText(vuelo.getBoardingGate().toString());
 
-                TextView estadoTV = findViewById(R.id.usersupport_result_estado_de_vuelo);
+                TextView estadoTV;
+                estadoTV = findViewById(R.id.usersupport_result_estado_de_vuelo);
                 estadoTV.setText(vuelo.getStateFlight().getDescription());
+
+                Log.d("------ UserSupport", vuelo.getStateFlight().getDescription());
+
+                if (vuelo.getStateFlight().getDescription().equalsIgnoreCase("CANCELADO")) {
+                    estadoTV.setTextColor(getResources().getColor(R.color.colorCancelado));
+                } else if (vuelo.getStateFlight().getDescription().equalsIgnoreCase("EN HORARIO")) {
+                    estadoTV.setTextColor(getResources().getColor(R.color.colorEnHorario));
+                } else if (vuelo.getStateFlight().getDescription().equalsIgnoreCase("DEMORADO")) {
+                    estadoTV.setTextColor(getResources().getColor(R.color.colorDemorado));
+                } else {
+                    estadoTV.setTextColor(getResources().getColor(R.color.colorBeacon1));
+                }
 
             }
 
@@ -98,10 +119,8 @@ public class UserSupportActivity extends AppCompatActivity {
             if (jsonArrayResponse == null) {
                 Toast.makeText(getApplicationContext(), "Null", Toast.LENGTH_LONG).show(); //FIXME
             } else if (jsonArrayResponse.getStatus() == 200) {
-                Toast.makeText(getApplicationContext(), "Done!", Toast.LENGTH_LONG).show();
-
+                //Toast.makeText(getApplicationContext(), "Done!", Toast.LENGTH_LONG).show();
                 cargarVuelosEnSpinner();
-
             } else {
                 Toast.makeText(getApplicationContext(), "Se obtuvo el error " + jsonArrayResponse.getStatus(), Toast.LENGTH_LONG).show(); //FIXME
             }
