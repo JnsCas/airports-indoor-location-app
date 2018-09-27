60da15b35a1acdd10052a25c2c0102b9928affa6
JnsCas <jnss.cas@gmail.com>
2018-09-27 20:31:01 -0300

    init notifications

--- app/src/main/java/afinal/proyecto/cuatro/grupo/airportsindoorlocationapp/MainActivity.java
+++ app/src/main/java/afinal/proyecto/cuatro/grupo/airportsindoorlocationapp/MainActivity.java
@@ -3,11 +3,20 @@ package afinal.proyecto.cuatro.grupo.airportsindoorlocationapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.estimote.mustard.rx_goodness.rx_requirements_wizard.Requirement;
+import com.estimote.mustard.rx_goodness.rx_requirements_wizard.RequirementsWizardFactory;
+
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import afinal.proyecto.cuatro.grupo.airportsindoorlocationapp.activities.LoginActivity;
+import afinal.proyecto.cuatro.grupo.airportsindoorlocationapp.application.MyApplication;
+import kotlin.Unit;
+import kotlin.jvm.functions.Function0;
+import kotlin.jvm.functions.Function1;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        requirementsEstimote();
 
         int timeout = 10; // make the activity visible for 10 miliseconds
 
@@ -30,5 +41,36 @@ public class MainActivity extends AppCompatActivity {
             }
         }, timeout);
 
+    }
+
+    private void requirementsEstimote() {
+        final MyApplication application = (MyApplication) getApplication();
+
+        RequirementsWizardFactory
+            .createEstimoteRequirementsWizard()
+            .fulfillRequirements(this,
+                new Function0<Unit>() {
+                    @Override
+                    public Unit invoke() {
+                    Log.d("app", "requirements fulfilled");
+                    application.enableBeaconNotifications();
+                    return null;
+                    }
+                },
+                new Function1<List<? extends Requirement>, Unit>() {
+                    @Override
+                    public Unit invoke(List<? extends Requirement> requirements) {
+                    Log.e("app", "requirements missing: " + requirements);
+                    return null;
+                    }
+                },
+                new Function1<Throwable, Unit>() {
+                    @Override
+                    public Unit invoke(Throwable throwable) {
+                        Log.e("app", "requirements error: " + throwable);
+                        return null;
+                    }
+                }
+            );
     }
 }
---
+++ app/src/main/java/afinal/proyecto/cuatro/grupo/airportsindoorlocationapp/notifications/NotificationsManager.java
@@ -0,0 +1,156 @@
+package afinal.proyecto.cuatro.grupo.airportsindoorlocationapp.notifications;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.estimote.proximity_sdk.api.ProximityObserver;
+import com.estimote.proximity_sdk.api.ProximityObserverBuilder;
+import com.estimote.proximity_sdk.api.ProximityZone;
+import com.estimote.proximity_sdk.api.ProximityZoneBuilder;
+import com.estimote.proximity_sdk.api.ProximityZoneContext;
+
+import java.util.Arrays;
+
+import afinal.proyecto.cuatro.grupo.airportsindoorlocationapp.MainActivity;
+import afinal.proyecto.cuatro.grupo.airportsindoorlocationapp.application.MyApplication;
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+
+public class NotificationsManager {
+
+    private Context context;
+    private NotificationManager notificationManager;
+    private int notificationIdLemon1 = 1;
+    private int notificationIdBeetroot1 = 2;
+    private int notificationIdCoconut1 = 3;
+    private int notificationIdCandy1 = 4;
+
+    public NotificationsManager(Context context) {
+        this.context = context;
+        this.notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    private Notification buildNotification(String title, String text) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel contentChannel = new NotificationChannel(
+                    "content_channel", "Things near you", NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(contentChannel);
+        }
+
+        return new NotificationCompat.Builder(context, "content_channel")
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(text)
+                .setContentIntent(PendingIntent.getActivity(context, 0,
+                        new Intent(context, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .build();
+    }
+
+    public void startMonitoring() {
+        ProximityObserver proximityObserver =
+                new ProximityObserverBuilder(context, ((MyApplication) context).cloudCredentials)
+                        .onError(new Function1<Throwable, Unit>() {
+                            @Override
+                            public Unit invoke(Throwable throwable) {
+                                Log.e("app", "proximity observer error: " + throwable);
+                                return null;
+                            }
+                        })
+                        .withBalancedPowerMode()
+                        .build();
+
+        ProximityZone zoneLemon1 = new ProximityZoneBuilder()
+                .forTag("notification-lemon-1")
+                .inCustomRange(3.0)
+                .onEnter(new Function1<ProximityZoneContext, Unit>() {
+                    @Override
+                    public Unit invoke(ProximityZoneContext proximityContext) {
+                        notificationManager.notify(notificationIdLemon1, buildNotification("Hello",
+                                "You're near your lemon1 beacon"));
+                        return null;
+                    }
+                })
+                .onExit(new Function1<ProximityZoneContext, Unit>() {
+                    @Override
+                    public Unit invoke(ProximityZoneContext proximityContext) {
+                        notificationManager.notify(notificationIdLemon1, buildNotification("Bye bye",
+                                "You've left the proximity of your lemon1 beacon"));
+                        return null;
+                    }
+                })
+                .build();
+
+        ProximityZone zoneBeetroot1 = new ProximityZoneBuilder()
+                .forTag("notification-beetroot-1") //FIXME Checkear en el cloud, esta seteado Estimote Monitoring -> off
+                .inCustomRange(3.0)
+                .onEnter(new Function1<ProximityZoneContext, Unit>() {
+                    @Override
+                    public Unit invoke(ProximityZoneContext proximityContext) {
+                        notificationManager.notify(notificationIdBeetroot1, buildNotification("Hello",
+                                "You're near your beetroot1 beacon"));
+                        return null;
+                    }
+                })
+                .onExit(new Function1<ProximityZoneContext, Unit>() {
+                    @Override
+                    public Unit invoke(ProximityZoneContext proximityContext) {
+                        notificationManager.notify(notificationIdBeetroot1, buildNotification("Bye bye",
+                                "You've left the proximity of your beetroot1 beacon"));
+                        return null;
+                    }
+                })
+                .build();
+
+        ProximityZone zoneCoconut1 = new ProximityZoneBuilder()
+                .forTag("notification-coconut-1")
+                .inCustomRange(3.0)
+                .onEnter(new Function1<ProximityZoneContext, Unit>() {
+                    @Override
+                    public Unit invoke(ProximityZoneContext proximityContext) {
+                        notificationManager.notify(notificationIdCoconut1, buildNotification("Hello",
+                                "You're near your coconut1 beacon"));
+                        return null;
+                    }
+                })
+                .onExit(new Function1<ProximityZoneContext, Unit>() {
+                    @Override
+                    public Unit invoke(ProximityZoneContext proximityContext) {
+                        notificationManager.notify(notificationIdCoconut1, buildNotification("Bye bye",
+                                "You've left the proximity of your coconut1 beacon"));
+                        return null;
+                    }
+                })
+                .build();
+
+        ProximityZone zoneCandy1 = new ProximityZoneBuilder()
+                .forTag("notification-candy-1")
+                .inCustomRange(3.0)
+                .onEnter(new Function1<ProximityZoneContext, Unit>() {
+                    @Override
+                    public Unit invoke(ProximityZoneContext proximityContext) {
+                        notificationManager.notify(notificationIdCandy1, buildNotification("Hello",
+                                "You're near your candy1 beacon"));
+                        return null;
+                    }
+                })
+                .onExit(new Function1<ProximityZoneContext, Unit>() {
+                    @Override
+                    public Unit invoke(ProximityZoneContext proximityContext) {
+                        notificationManager.notify(notificationIdCandy1, buildNotification("Bye bye",
+                                "You've left the proximity of your candy1 beacon"));
+                        return null;
+                    }
+                })
+                .build();
+
+        proximityObserver.startObserving(Arrays.asList(zoneLemon1, zoneBeetroot1, zoneCandy1, zoneCoconut1));
+    }
+
+}
