c7d1c0f9d2c6c90cace307fc74d4e6ae225805fb
evincelli <ernesto.vincelli@mercadolibre.com>
2018-12-02 14:25:07 -0300

    ajuste de layout con un mismo formato

--- app/src/main/java/afinal/proyecto/cuatro/grupo/airportsindoorlocationapp/activities/InfoActivity.java
+++ app/src/main/java/afinal/proyecto/cuatro/grupo/airportsindoorlocationapp/activities/InfoActivity.java
@@ -1,9 +1,12 @@
 package afinal.proyecto.cuatro.grupo.airportsindoorlocationapp.activities;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -20,16 +23,20 @@ import afinal.proyecto.cuatro.grupo.airportsindoorlocationapp.util.JsonObjectRes
 
 public class InfoActivity extends AppCompatActivity {
 
-    private TextView resultTV;
-    private TextView resultDuracionEstadiaTV;
+    private TextView resultTV1;
+    private TextView resultTV2;
+    private TextView resultDuracionEstadiaPE1;
+    private TextView resultDuracionEstadiaPE2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
-        resultTV = findViewById(R.id.info_people_quantity_result);
-        resultDuracionEstadiaTV = findViewById(R.id.info_duracion_estadia);
+        resultTV1 = findViewById(R.id.info_people_quantity_result1);
+        resultTV2 = findViewById(R.id.info_people_quantity_result2);
+        resultDuracionEstadiaPE1 = findViewById(R.id.info_duracion_estadia1);
+        resultDuracionEstadiaPE2 = findViewById(R.id.info_duracion_estadia2);
 
         new PostPeopleQuantity().execute();
         new GetEsperaActual().execute();
@@ -39,7 +46,8 @@ public class InfoActivity extends AppCompatActivity {
     private class PostPeopleQuantity extends AsyncTask<Void, Void, Void> {
 
         private ProgressDialog progressDialog;
-        private JsonObjectResponse response;
+        private JsonObjectResponse responsePE1;
+        private JsonObjectResponse responsePE2;
 
         public PostPeopleQuantity() {
             this.progressDialog = new ProgressDialog(InfoActivity.this);
@@ -48,21 +56,31 @@ public class InfoActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             progressDialog.setTitle("Espere por favor");
-            progressDialog.setMessage("Enviando datos..");
+            progressDialog.setMessage("Enviando datos...");
             progressDialog.show();
         }
 
         @Override
         protected Void doInBackground(Void... voids) {
-            Map<String, String> requestParam = new HashMap<>();
-            requestParam.put("zone", "lemon-2"); //FIXME hardcodeo fuerte. lemon-2 es la puerta de embarque
+
             Calendar fiveMinutesBefore = Calendar.getInstance();
             fiveMinutesBefore.add(Calendar.MINUTE, -5);
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            requestParam.put("since", sdf.format(fiveMinutesBefore.getTime()));
-            //requestParam.put("until", "");
-            response = ConexionWebService
-                    .getJsonObject("/flowAnalysis/peopleQuantity", requestParam);
+
+
+            Map<String, String> requestParamPE1 = new HashMap<>();
+            requestParamPE1.put("zone", "beetroot-1"); //FIXME: Hardcodeo fuerte
+            requestParamPE1.put("since", sdf.format(fiveMinutesBefore.getTime()));
+            responsePE1 = ConexionWebService.getJsonObject("/flowAnalysis/peopleQuantity", requestParamPE1);
+
+            Map<String, String> requestParamPE2 = new HashMap<>();
+            requestParamPE2.put("zone", "candy-1"); //FIXME: Hardcodeo fuerte
+            requestParamPE2.put("since", sdf.format(fiveMinutesBefore.getTime()));
+
+            System.out.println(">>>> requestParamPE1: " + requestParamPE1);
+
+            responsePE2 = ConexionWebService.getJsonObject("/flowAnalysis/peopleQuantity", requestParamPE2);
+
             return null;
         }
 
@@ -74,28 +92,41 @@ public class InfoActivity extends AppCompatActivity {
             }
 
             try {
-                int result = response.getJsonObject().getInt("peopleQuantity");
-                resultTV.setText(String.valueOf(result));
+                int result1 = responsePE1.getJsonObject().getInt("peopleQuantity");
+                resultTV1.setText(String.valueOf(result1));
+
+                int result2 = responsePE2.getJsonObject().getInt("peopleQuantity");
+                resultTV2.setText(String.valueOf(result2));
+
+                Log.d("---> InfoActivity","> peopleQuantity1: " + result1 + "> peopleQuantity2: " + result2);
+
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
     }
 
+    @SuppressLint("StaticFieldLeak")
     private class GetEsperaActual extends AsyncTask<Void, Void, Void> {
 
         private ProgressDialog progressDialog;
-        private JsonObjectResponse response;
+        private JsonObjectResponse responsePE1;
+        private JsonObjectResponse responsePE2;
 
-        public GetEsperaActual() {
+        GetEsperaActual() {
             this.progressDialog = new ProgressDialog(InfoActivity.this);
         }
 
         @Override
         protected Void doInBackground(Void... voids) {
-            Map<String, String> requestParam = new HashMap<>();
-            requestParam.put("zone", "lemon-2"); //FIXME hardcodeo fuerte. lemon-2 es la puerta de embarque
-            response = ConexionWebService.getJsonObject("/flowAnalysis/duracionEstadia", requestParam);
+            Map<String, String> requestParamPE1 = new HashMap<>();
+            requestParamPE1.put("zone", "br1"); //FIXME: Hardcodeo fuerte
+            responsePE1 = ConexionWebService.getJsonObject("/duracionEstadia", requestParamPE1);
+
+            Map<String, String> requestParamPE2 = new HashMap<>();
+            requestParamPE2.put("zone", "ca1"); //FIXME: Hardcodeo fuerte
+            responsePE2 = ConexionWebService.getJsonObject("/duracionEstadia", requestParamPE2);
+
             return null;
         }
 
@@ -105,11 +136,19 @@ public class InfoActivity extends AppCompatActivity {
             if (progressDialog.isShowing()) {
                 progressDialog.dismiss();
             }
-
             try {
-                Double result = response.getJsonObject().getDouble("duracionEstadia");
-                String msgResult= String.format("%.2f", result) + " Minutos";
-                resultDuracionEstadiaTV.setText(msgResult);
+                Double resultPE1 = responsePE1.getJsonObject().getDouble("duracionEstadia");
+                @SuppressLint("DefaultLocale") String msgResult1= String.format("%.2f", resultPE1) + " Minutos";
+                resultDuracionEstadiaPE1.setText(msgResult1);
+                resultDuracionEstadiaPE1.setTypeface(null, Typeface.BOLD);
+
+                Double resultPE2 = responsePE2.getJsonObject().getDouble("duracionEstadia");
+                @SuppressLint("DefaultLocale") String msgResult2= String.format("%.2f", resultPE2) + " Minutos";
+                resultDuracionEstadiaPE2.setText(msgResult2);
+                resultDuracionEstadiaPE2.setTypeface(null, Typeface.BOLD);
+
+                Log.d("---> InfoActivity","> msgResult1: " + msgResult1 + " > msgResult2: " + msgResult2);
+
             } catch (JSONException e) {
                 e.printStackTrace();
             }
